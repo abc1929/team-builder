1b50859e0b2f076753932d764131a370b1a0b7ff
Jiawei Wu <abc1929@gmail.com>
2021-01-27 08:13:21 +0800

    added multiple teams functionality

--- team-builder-app/src/App.js
+++ team-builder-app/src/App.js
@@ -1,29 +1,82 @@
 import React, { useState, useEffect } from "react";
 import Team from "./Components/team";
 import Form from "./Components/form";
-import { ChakraProvider } from "@chakra-ui/react";
+import { IconButton, ChakraProvider } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import "./App.css";
 
 function App() {
-   const [team, setTeam] = useState([]);
+   const [currentteam, setCurrentteam] = useState([]);
+   const [currenttab, setCurrenttab] = useState(1);
+   const [teams, setTeams] = useState([[1, []]]);
    const [memberToEdit, setMemberToEdit] = useState(null);
 
-   useEffect(() => {}, [memberToEdit]);
+   //  useEffect(() => {
+   //     const newteams = [...teams];
+   //     newteams[currenttab - 1] = [currenttab, currentteam];
+   //     setTeams(newteams);
+   //  }, [currentteam]);
+
+   const updateall = () => {
+      const newteams = [...teams];
+      newteams[currenttab - 1] = [currenttab, currentteam];
+      setTeams(newteams);
+   };
 
    return (
       <ChakraProvider>
-         <div className="App">
-            <h1 style={{ fontSize: "2.4rem" }}> Team Application </h1>
-            <div className="main">
-               <Form
-                  team={team}
-                  setTeam={setTeam}
-                  memberToEdit={memberToEdit}
-                  setMemberToEdit={setMemberToEdit}
-               ></Form>
-               <Team team={team} setMemberToEdit={setMemberToEdit}></Team>
-            </div>
-         </div>
+         <Tabs>
+            <TabList>
+               {teams.map((i) => (
+                  <Tab
+                     key={"tab" + i[0]}
+                     onClick={() => {
+                        updateall();
+                        setCurrentteam(teams[i[0] - 1][1]);
+                        setCurrenttab(i[0]);
+                     }}
+                  >
+                     {i[0]}
+                  </Tab>
+               ))}
+               <IconButton
+                  icon={<AddIcon />}
+                  margin="1% 2%"
+                  size="sm"
+                  onClick={(e) => {
+                     const newteam = [teams[teams.length - 1][0] + 1, []];
+                     setTeams(teams.concat([newteam]));
+                  }}
+               ></IconButton>
+            </TabList>
+            <TabPanels>
+               {teams.map((i) => {
+                  return (
+                     <TabPanel key={"tabpanel" + i[0]}>
+                        <div className="App">
+                           <h1 style={{ fontSize: "2.4rem" }}>
+                              {" "}
+                              Team Application{" "}
+                           </h1>
+                           <div className="main">
+                              <Form
+                                 team={currentteam}
+                                 setTeam={setCurrentteam}
+                                 memberToEdit={memberToEdit}
+                                 setMemberToEdit={setMemberToEdit}
+                              ></Form>
+                              <Team
+                                 team={currentteam}
+                                 setMemberToEdit={setMemberToEdit}
+                              ></Team>
+                           </div>
+                        </div>
+                     </TabPanel>
+                  );
+               })}
+            </TabPanels>
+         </Tabs>
       </ChakraProvider>
    );
 }
--- team-builder-app/src/Components/team.js
+++ team-builder-app/src/Components/team.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Member from "./member";
 // import { Divider } from "@chakra-ui/react";
 
 export default function Team(props) {
    // const [t, setT] = useState(aa);
 
+   // const [team, setTeam] = useState([]);
+
    return (
       <div className="team">
+         {/* {console.log(props.team)} */}
          {props.team.map((i) => {
             return (
                <div key={i.email}>
